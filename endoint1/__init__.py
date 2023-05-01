aa7e038e2697a165162078619cd22026aa63b731
Emanuele <emanuele.pecorari@gmail.com>
2023-05-01 23:27:30 +0200

    v20

--- endoint1/__init__.py
+++ endoint1/__init__.py
@@ -16,9 +16,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"v19 Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"v20 Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "v19 This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "v20 This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
